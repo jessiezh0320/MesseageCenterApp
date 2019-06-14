58deea582f26a89f9c5807bddb3e80b7e9338d6f
Jessie <michelle3208@163.com>
2019-06-14 18:26:50 +0800

    haha

---
+++ Interactivity/interactivityListView/interactivityListView/Views/DetailPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace interactivityListView.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DetailPage : ContentPage
+	{
+		public DetailPage ()
+		{
+			InitializeComponent ();
+
+            
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            MessagingCenter.Subscribe<interactiveListViewXaml, string>(this, "Invoke", async (sender, arg) =>
+            {
+
+                Debug.Write("123456---->  get one msg");
+            });
+
+            //MessagingCenter.Subscribe<object, object>(this, "Invoke", (sender, arg) =>
+            //{
+            //    //await Process(arg);
+            //    Debug.Write("123456---->  get one msg");
+            //});
+        }
+
+        protected async override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            Debug.Write("123456---->  OnDisappearing() is called.............");
+            //MessagingCenter.Unsubscribe<object, object>(this, "Invoke");
+
+            MessagingCenter.Unsubscribe<interactiveListViewXaml,string>(this, "Invoke");
+
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ Interactivity/interactivityListView/interactivityListView/Views/interactiveListViewXaml.xaml.cs
@@ -0,0 +1,74 @@
+﻿using interactivityListView.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace interactivityListView
+{
+	public partial class interactiveListViewXaml : ContentPage
+	{
+		public static ObservableCollection<string> items { get; set; }
+
+		public interactiveListViewXaml ()
+		{
+            items = new ObservableCollection<string> () { "Speaker", "Pen", "Lamp", "Monitor", "Bag", "Book", "Cap", "Tote", "Floss", "Phone"};
+			InitializeComponent ();
+		}
+
+		//void OnSelection (object sender, SelectedItemChangedEventArgs e)
+		//{
+		//	if (e.SelectedItem == null) 
+  //          {
+		//		return;
+		//	}
+		//	DisplayAlert ("Item Selected", e.SelectedItem.ToString (), "Ok");
+		//}
+
+		void OnRefresh (object sender, EventArgs e)
+		{
+			var list = (ListView)sender;
+			//put your refreshing logic here
+			var itemList = items.Reverse().ToList();
+			items.Clear ();
+			foreach (var s in itemList) {
+				items.Add (s);
+			}
+			//make sure to end the refresh state
+			list.IsRefreshing = false;
+		}
+
+      
+
+        async void OnTap (object sender, EventArgs e)//ItemTappedEventArgs   //EventArgs
+        {
+            await Navigation.PushAsync(new DetailPage());
+            try
+            {
+                MessagingCenter.Send(this, "Invoke", "Invokedtrue");
+                //MessagingCenter.Send<object, object>(this, "Invoke", "Invokedtrue");
+                Debug.Write("123456---->  send one msg");
+
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+            }
+
+        }
+
+		void OnMore (object sender, EventArgs e)
+		{
+			var item = (MenuItem)sender;
+			DisplayAlert("More Context Action", item.CommandParameter + " more context action", "OK");
+		}
+
+		void OnDelete (object sender, EventArgs e)
+		{
+			var item = (MenuItem)sender;
+			items.Remove (item.CommandParameter.ToString());
+		}
+	}
+}
+
